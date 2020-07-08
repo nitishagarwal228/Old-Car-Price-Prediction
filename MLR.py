3b2e8c5541815f3d890169fd674d8a30aa554cae
nitishagarwal228 <68009434+nitishagarwal228@users.noreply.github.com>
2020-07-08 19:27:14 +0530

    Add files via upload

--- MLR.py
+++ MLR.py
@@ -8,7 +8,7 @@ import datetime
 
 data = pd.read_csv('car.csv')
 
-data_x = data.iloc[:,[1,3,4,5,6,7,8]]
+data_x = data.iloc[:,[1,3,4,5,6,7]]
 data_y = data.iloc[:,2]
 
 x= datetime.datetime.now()
@@ -18,11 +18,9 @@ Transmission={"Automatic" : 1, "Manual" : 2}
 Seller_Type={"Dealer" : 1, "Individual" : 2}
 
 
-
-
 data_x=data_x.replace({"Fuel_Type" : fuel_type, "Transmission" : Transmission,"Seller_Type": Seller_Type})
 data_x=data_x.values
-data_x[:,0]=x.year-data_x[:,0]
+data_x[:,0]=2003-data_x[:,0]
 
 
 X_train,X_test,y_train,y_test=train_test_split(data_x,data_y,test_size=0.3,random_state=0)
@@ -31,7 +29,8 @@ reg = LinearRegression()
 reg.fit(X_train,y_train)
 
 print("Train Score: ",reg.score(X_train,y_train))
-print("Test Score : ",reg.score(X_test,y_test))
+
 
 pickle.dump(reg, open('car.pkl','wb'))
+
 
